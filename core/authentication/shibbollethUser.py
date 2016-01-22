62163e6daade3f38e34e7cbd3b345e0ae959fb06
Gaëtan La Marca <gaetan.lamarca@uclouvain.be>
2016-01-22 02:37:30 +0100

    #79 logout

--- core/authentication/shibbollethUser.py
+++ core/authentication/shibbollethUser.py
@@ -33,7 +33,7 @@ from django.contrib.sessions.models import Session
 from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import reverse
 from django.http.response import HttpResponseRedirect
-from django.middleware import cache
+from django.core.cache import cache
 from pip._vendor.requests.packages import urllib3
 from osis_backend import settings
 
